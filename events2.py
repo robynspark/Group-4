f8f146e6361ff20f53cd45c010d61c7cd94b7e0f
hswillwerth <hswillwerth@gwu.edu>
2017-04-22 17:16:24 -0400

    Use these!
    Final version of the scrapers.

--- events2.py
+++ events2.py
@@ -15,31 +15,15 @@ table = soup.find('table', id="tblEvents")
 list_of_rows = []
 for row in table.findAll('tr')[2:]:
     list_of_cells = []
-#    for url in row.findAll('href'):
-#        list_of_cells.append.a.get_text(url)
-#    for cell in row.findAll('td'):
-#        list_of_cells.append(cell.text.encode('utf-8'))
-#    list_of_rows.append(list_of_cells)    
-    for cell in row.findAll('td'):
-        if cell.find('a'):
-        	print cell.find('a').attrs
-        	if cell.find('a').has_attr('href'):
-				link = cell.find('a')['href']
-				list_of_cells.append(link)
-				list_of_cells.append(cell.html)
-        else:
-            list_of_cells.append(cell.html)
+    # since each row has only two TD tags (cells), I assigned them to the result of row.findAll('td') and called them first_cell and second_cell
+    # since we know that href links are always in the second cell, we can just reference it directly. You'll need to make the url a full path.
+    first_cell, second_cell = row.findAll('td')
+    list_of_cells.append(first_cell.find('a').text)
+    list_of_cells.append("http://browse.calendar.gwu.edu/"+second_cell.find('a')['href'])
+    list_of_cells.append(second_cell.text)
     list_of_rows.append(list_of_cells)
 
 outfile = open("events2.csv", "wb")
 writer = csv.writer(outfile)
 writer.writerow(["time", "url", "text"])
 writer.writerows(list_of_rows)
-
-
-
-
-
-
-
-
